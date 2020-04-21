59a890027adf61bcaecdf71f57c3d77eddc9b801
CherryIce <a13530836164@163.com>
2020-04-21 10:21:10 +0800

    loadhtml

--- Configures/ViewController.h
+++ Configures/ViewController.h
@@ -8,6 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
+#import <WebKit/WebKit.h>
+
 @interface ViewController : UIViewController
 
 
--- Configures/ViewController.m
+++ Configures/ViewController.m
@@ -21,9 +21,17 @@
 #define NSLog(args, ...)
 #endif
 
-@interface ViewController ()
+@interface ViewController ()<WKScriptMessageHandler,WKUIDelegate,WKNavigationDelegate>
 
+/**
+ dict->model
+ */
 @property(nonatomic,copy)NSDictionary * dict;
+
+/**
+ WKWebView
+ */
+@property (nonatomic,strong)WKWebView  *wkWebView;
 
 @end
 
@@ -36,6 +44,7 @@
    // [self test1];
    // [self test2];
     [self modelTest];
+    [self loadLocalHtmlAction];
 }
 
 #pragma mark -- test1
@@ -113,5 +122,66 @@
     return _dict;
 }
 
+#pragma mark 加载本地HTML操作(index)
+-(void)loadLocalHtmlAction{
+    //主要是文件路径需要选蓝色文件夹 😂😂😂
+    NSURL *urlObj = [[NSBundle mainBundle] URLForResource:@"LocalData/Html/dist/index.html" withExtension:nil];
+    if (!urlObj) { return; }
+    NSString * urlStr = [NSString stringWithFormat:@"%@#/home/cn",urlObj.absoluteString];
+    urlObj = [NSURL URLWithString:urlStr];
+    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:urlObj];
+    [self.wkWebView loadRequest:urlRequest];
+}
+
+#pragma mark WKWebView
+- (WKWebView *)wkWebView {
+    if (!_wkWebView) {
+        WKWebViewConfiguration *config=[[WKWebViewConfiguration alloc]init];
+        config.preferences=[WKPreferences new];
+        config.preferences.minimumFontSize=10;
+        config.preferences.javaScriptCanOpenWindowsAutomatically=true;
+        config.preferences.javaScriptEnabled=YES;  // 设置可以JS交互
+        [config.preferences setValue:@YES forKey:@"allowFileAccessFromFileURLs"];
+        
+        WKUserContentController *wkUserVC=[[WKUserContentController alloc]init];
+        config.userContentController=wkUserVC;
+        
+        CGRect rectValue = self.view.bounds;
+        _wkWebView=[[WKWebView alloc]initWithFrame:rectValue configuration:config];
+        _wkWebView.scrollView.showsVerticalScrollIndicator=NO;
+        _wkWebView.scrollView.showsHorizontalScrollIndicator=NO;
+        _wkWebView.allowsBackForwardNavigationGestures=YES;
+        _wkWebView.UIDelegate=self;
+        _wkWebView.navigationDelegate=self;
+        [self.view addSubview:_wkWebView];
+    }
+    return _wkWebView;
+}
+
+#pragma mark -WKNavigationDelegate
+// 拦截URL操作
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler{
+    // 允许加载
+    decisionHandler(WKNavigationActionPolicyAllow);
+}
+
+// 加载完成
+-(void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation{
+    
+}
+
+- (void)webView:(WKWebView *)webView didFailNavigation:(null_unspecified WKNavigation *)navigation withError:(NSError *)error{
+    NSLog(@"reson1加载网页失败,原因是: %@",error);
+}
+- (void)webView:(WKWebView *)webView didFailProvisionalNavigation:(null_unspecified WKNavigation *)navigation withError:(NSError *)error{
+    NSLog(@"reson2加载网页失败,原因是: %@",error);
+}
+
+#pragma mark WKScriptMessageHandler
+// JS调用OC的回调
+-(void)userContentController:(WKUserContentController *)userContentController didReceiveScriptMessage:(WKScriptMessage *)message{
+
+//    NSDictionary *dicBodyValue=message.body;
+}
 
 @end
